b9dc96e566b6d63459c003b4be46833d05bb73a9
Kim <121239353+youfoundKim@users.noreply.github.com>
2023-03-18 20:16:43 +0100

    Middleware example

---
+++ examples/middleware.js
@@ -0,0 +1,27 @@
+import Pronto from "../index.js";
+
+const server = new Pronto();
+
+// Middleware 1
+server.use("/", (ctx, next) => {
+  ctx.header("Test-Header-1", "Middleware 1");
+  next();
+});
+
+// Middleware 2
+server.use("/", (ctx, next) => {
+  ctx.header("Test-Header-2", "Middleware 2");
+  next();
+});
+
+// Add two handlers
+server.get("/", (ctx, next) => {
+  ctx.write("Hello");
+  next();
+}, (ctx) => {
+  ctx.end(", world!");
+});
+
+server.listen(3000, (socket) => {
+  console.log("http://localhost:3000");
+});
--- src/websocket.js
+++
@@ -1,5 +0,0 @@
-export default class WebSocket {
-  constructor(websocket) {
-    this.websocket = websocket;
-  }
-}
